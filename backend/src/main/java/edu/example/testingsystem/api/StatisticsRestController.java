72952eea2b980e3aefdcf2c849b62279e603c856
Gmakk <71002826+Gmakk@users.noreply.github.com>
2024-12-07 17:05:20 +0300

    Statistics controller

--- backend/src/main/java/edu/example/testingsystem/api/StatisticsRestController.java
+++ backend/src/main/java/edu/example/testingsystem/api/StatisticsRestController.java
@@ -1,5 +1,52 @@
 package edu.example.testingsystem.api;
 
-//TODO:
+import edu.example.testingsystem.controllers.statistics.ProjectStatistics;
+import edu.example.testingsystem.controllers.statistics.StatisticsCollector;
+import edu.example.testingsystem.entities.Project;
+import edu.example.testingsystem.repos.ProjectRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/statistics")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
 public class StatisticsRestController {
+
+    StatisticsCollector collector;
+    ProjectRepository projectRepository;
+
+    @GetMapping("/project/{title}")
+    public ResponseEntity<Object> statisticsByProject(@PathVariable("title") String title) {
+        Optional<Project> optionalProject = projectRepository.findById(title);
+        if(optionalProject.isEmpty())
+            return ResponseEntity.notFound().build();
+        Project project = optionalProject.get();
+
+        List<ProjectStatistics> projectStatisticsList = collector.collectStatisticsByProject(project);
+        String jsonString = collector.constructJsonString(projectStatisticsList);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("text/plain"))
+                .body(jsonString.getBytes());
+    }
+
+    @GetMapping()
+    public ResponseEntity<Object> allStatistics() {
+        List<ProjectStatistics> projectStatisticsList = collector.collectStatistics();
+        String jsonString = collector.constructJsonString(projectStatisticsList);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("text/plain"))
+                .body(jsonString.getBytes());
+    }
 }
