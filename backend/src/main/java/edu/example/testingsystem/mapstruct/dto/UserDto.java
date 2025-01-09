5b58ed60a89908330f74a5fd6be98b7f768c4d23
Gmakk <71002826+Gmakk@users.noreply.github.com>
2025-01-09 18:10:31 +0300

    All user fields in response

--- backend/src/main/java/edu/example/testingsystem/mapstruct/dto/UserDto.java
+++ backend/src/main/java/edu/example/testingsystem/mapstruct/dto/UserDto.java
@@ -3,7 +3,7 @@ package edu.example.testingsystem.mapstruct.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import edu.example.testingsystem.entities.Role;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+//@JsonInclude(JsonInclude.Include.NON_NULL)
 public record UserDto(Integer id,
                       String fullName,
                       String login,
