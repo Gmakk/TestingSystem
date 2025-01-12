fb795261e5a60efb7c7684ee6470686d67e3192b
Gmakk <71002826+Gmakk@users.noreply.github.com>
2025-01-12 22:16:31 +0300

    Temporary integration endpoint realization for development purposes

---
+++ backend/src/main/java/edu/example/testingsystem/api/IntegrationRestController.java
@@ -0,0 +1,26 @@
+package edu.example.testingsystem.api;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.Charset;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/api/integration")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class IntegrationRestController {
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateDescriptionByTitle(@RequestBody String title) {
+        byte[] array = new byte[20]; // length is bounded by 7
+        new Random().nextBytes(array);
+        String generatedString = new String(array, Charset.forName("UTF-16"));
+
+        return ResponseEntity.ok(generatedString);
+    }
+}
